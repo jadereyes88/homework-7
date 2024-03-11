f9ae4868627024a13cbd32eb2b08f07329950ee1
jadereyes88 <122693715+jadereyes88@users.noreply.github.com>
2024-03-11 18:29:22 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,14 @@ namespace app\models;
 
 class Post
 {
+      private $posts = [
+        ['id' => 1, 'title' => 'Post One', 'content' => 'Content for post one.'],
+        ['id' => 2, 'title' => 'Post Two', 'content' => 'Content for post two.'],
+        ['id' => 3, 'title' => 'Post Three', 'content' => 'Content for post three.'],
+    ];
 
+    public function getAll()
+    {
+        return $this->posts;
+    }
 }
