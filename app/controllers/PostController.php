2e4412e3de13a24aaab4b55f1a7abcae78e00230
jadereyes88 <122693715+jadereyes88@users.noreply.github.com>
2024-03-11 18:28:27 -0400

    Update PostController.php

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -4,9 +4,14 @@ namespace app\controllers;
 use app\core\Controller;
 class PostController extends Controller
 {
-//todo make a method to return some posts, post objects should come from the post model class
-//also need to make a twig template to show the posts
-//an example is in app/controllers/UsersController
 
+    public function index()
+    {
+        $postModel = new Post();
+        $posts = $postModel->getAll(); // Assume getAll() fetches all posts.
+
+        $template = $this->twig->load('posts/index.twig'); // Load the Twig template.
+        echo $template->render(['posts' => $posts]); // Pass the posts to the template.
+    }
 
 }
